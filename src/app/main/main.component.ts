54f6bb23b251ac3f84bec1a4077f721c9e230392
Hex <reppeti@gmail.com>
2026-02-01 00:21:02 +0100

    adding more functionality

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../parts/header/header.component";
 import { RouterOutlet } from "@angular/router";
-
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from '../app.component';
+import { appConfig } from '../app.config';
+bootstrapApplication(AppComponent, appConfig);
 @Component({
   selector: 'app-main',
   standalone: true,
--- src/app/pages/smelting-xp/smelting-xp.component.ts
+++ src/app/pages/smelting-xp/smelting-xp.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { JsonLoaderService } from '../../services/json-loader.service';
 
 @Component({
   selector: 'app-smelting-xp',
   standalone: true,
   imports: [],
   templateUrl: './smelting-xp.component.html',
-  styleUrl: './smelting-xp.component.scss'
+  styleUrl: './smelting-xp.component.scss',
 })
 export class SmeltingXpComponent {
+  loadedRecipe: any[] = [];
+  jsonPath = "recipe/"
 
+  constructor(private jsonLoaderService: JsonLoaderService) {}
+
+  ngOnInit(): void {
+    this.jsonLoaderService.getJsonData("recipe/acacia_button.json").subscribe((data:any) => {
+      console.log(data)
+    });
+  }
 }
