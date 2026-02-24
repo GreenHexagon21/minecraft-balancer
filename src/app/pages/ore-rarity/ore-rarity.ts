106fe5d0a3abe1b36924c9b624511f6117be4c36
Hex <reppeti@gmail.com>
2026-02-24 22:34:42 +0100

    added local loading and structure

--- src/app/pages/ore-rarity/ore-rarity.ts
+++ src/app/pages/ore-rarity/ore-rarity.ts
@@ -5,25 +5,46 @@ import {
   MatSelectModule,
 } from '@angular/material/select';
 import { JsonLoaderService } from '../../services/json-loader.service';
-import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-ore-rarity',
-  imports: [MatSelect, MatSelectModule, FormsModule,MatInputModule, ReactiveFormsModule],
+  imports: [
+    MatSelect,
+    MatSelectModule,
+    FormsModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+  ],
   templateUrl: './ore-rarity.html',
   styleUrl: './ore-rarity.scss',
 })
 export class OreRarity {
   filtersForm: FormGroup;
-  constructor(private jsonLoaderService: JsonLoaderService,private fb: FormBuilder) {
+
+  constructor(
+    private jsonLoaderService: JsonLoaderService,
+    private fb: FormBuilder,
+  ) {
     this.filtersForm = this.fb.group({
-    rarityFilter: [''],
-    count: [null, [Validators.min(0)]],
-    maxHeight: [null, [Validators.min(0)]],
-    minHeight: [null, [Validators.min(0)]],
-    distributionShape: [''],
-  });
+      rarityFilter: [null],
+      count: [null, []],
+      maxHeight: [null, []],
+      minHeight: [null, []],
+      distributionShape: [null],
+    });
+  }
+
+  isVisible(controlName: string): boolean {
+    return this.filtersForm.get(controlName)?.value != null;
   }
 
   selectedOre: any;
@@ -34,6 +55,15 @@ export class OreRarity {
   min_height: any;
   min_type: any;
 
+  heightType = [
+    { value: 'minecraft:uniform', label: 'Universal' },
+    { value: 'minecraft:biased_to_bottom', label: 'Biased To Bottom' },
+    { value: 'minecraft:trapezoid', label: 'Trapezoid' },
+    {
+      value: 'minecraft:very_biased_to_bottom',
+      label: 'Very Biased To Bottom',
+    },
+  ];
 
   oreGeneration = [
     { value: 'ore_ancient_debris_large.json', label: 'Ancient debris (large)' },
@@ -76,55 +106,89 @@ export class OreRarity {
 
   oreSelectionChanged(event: MatSelectChange) {
     const localCopy = JSON.parse(localStorage.getItem(event.value) ?? '[]');
+
     if (localCopy.length != 0) {
       this.loadedRecipe = localCopy;
-      // this.xp = localCopy.experience;
-      // this.cookingTime = localCopy.cookingtime;
+      this.filtersForm.setValue(this.loadedRecipe);
     } else {
+      this.filtersForm.reset();
+
       this.jsonLoaderService
         .getJsonData(this.jsonPath + event.value)
         .subscribe((data: any) => {
           this.loadedRecipe = data;
-          // console.log(this.loadedRecipe);
+
           this.loadedRecipe.placement.forEach((element: any) => {
-            // console.log(element)
+            console.log(element);
+
+            if (element?.type == 'minecraft:rarity_filter') {
+              this.filtersForm
+                .get('rarityFilter')
+                ?.setValue(element?.chance ?? null);
+            }
+
+            if (element?.type == 'minecraft:count') {
+              this.filtersForm.get('count')?.setValue(element?.count ?? null);
+            }
+
             if (element?.type == 'minecraft:height_range') {
-              console.log(element?.height);
-              if (element?.height?.max_inclusive) {
-                if (element?.height?.max_inclusive?.absolute) {
-                  this.max_height = element?.height?.max_inclusive?.absolute;
-                  this.max_type = 'Absolute';
-                }
-                if (element?.height?.max_inclusive?.below_top) {
-                  this.max_height = element?.height?.max_inclusive?.below_top;
-                  this.max_type = 'Below top';
-                }
-                if (element?.height?.max_inclusive?.above_bottom) {
-                  this.max_height =
-                    element?.height?.max_inclusive?.above_bottom;
-                  this.max_type = 'Above bottom';
-                }
+              const max = element?.height?.max_inclusive;
+              if (max?.absolute != null) {
+                this.filtersForm.get('maxHeight')?.setValue(max.absolute);
+                this.max_type = 'Absolute';
+              } else if (max?.below_top != null) {
+                this.filtersForm.get('maxHeight')?.setValue(max.below_top);
+                this.max_type = 'Below top';
+              } else if (max?.above_bottom != null) {
+                this.filtersForm.get('maxHeight')?.setValue(max.above_bottom);
+                this.max_type = 'Above bottom';
               }
-              if (element?.height?.min_inclusive) {
-                if (element?.height?.min_inclusive?.absolute) {
-                  this.min_height = element?.height?.min_inclusive?.absolute;
-                  this.min_type = 'Absolute';
-                }
-                if (element?.height?.min_inclusive?.below_top) {
-                  this.min_height = element?.height?.min_inclusive?.below_top;
-                  this.min_type = 'Below top';
-                }
-                if (element?.height?.min_inclusive?.above_bottom) {
-                  this.min_height =
-                    element?.height?.min_inclusive?.above_bottom;
-                  this.min_type = 'Above bottom';
-                }
+
+              const min = element?.height?.min_inclusive;
+              if (min?.absolute != null) {
+                this.filtersForm.get('minHeight')?.setValue(min.absolute);
+                this.min_type = 'Absolute';
+              } else if (min?.below_top != null) {
+                this.filtersForm.get('minHeight')?.setValue(min.below_top);
+                this.min_type = 'Below top';
+              } else if (min?.above_bottom != null) {
+                this.filtersForm.get('minHeight')?.setValue(min.above_bottom);
+                this.min_type = 'Above bottom';
               }
             }
-            // this.xp = this.loadedRecipe.experience;
-            // this.cookingTime = this.loadedRecipe.cookingtime;
+
+            if (element?.height?.type) {
+              this.filtersForm
+                .get('distributionShape')
+                ?.setValue(element?.height?.type);
+            }
           });
         });
     }
+  }
+
+  save() {
+    let tempObj = this.filtersForm.value;
+
+    console.log(this.filtersForm.value);
+    localStorage.setItem(this.selectedOre, JSON.stringify(tempObj));
+
+    let tempItems: any[];
+    if (localStorage.getItem('saved-items')) {
+      tempItems = JSON.parse(localStorage.getItem('saved-items') ?? '[]');
+    } else {
+      tempItems = [];
+    }
+
+    tempItems.push(this.selectedOre);
+    let tempSet = new Set(tempItems);
+
+    localStorage.setItem(
+      'saved-items',
+      JSON.stringify(Array.from(tempSet.values())),
+    );
+
+    console.log(localStorage.getItem('saved-items'));
+    console.log(localStorage.getItem(this.selectedOre));
   }
 }
