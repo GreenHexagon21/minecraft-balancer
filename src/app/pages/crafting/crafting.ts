fc5cc522410ade092630363d184fbfd7abe994c6
Hex <reppeti@gmail.com>
2026-03-15 23:05:40 +0100

    added tag set and crafting

---
+++ src/app/pages/crafting/crafting.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crafting',
+  imports: [],
+  templateUrl: './crafting.html',
+  styleUrl: './crafting.scss',
+})
+export class Crafting {
+
+}
--- src/app/pages/tag-sets/tag-sets.ts
+++ src/app/pages/tag-sets/tag-sets.ts
@@ -1,19 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Globals } from '../../services/globals';
 import { JsonLoaderService } from '../../services/json-loader.service';
 import { SelectModule } from 'primeng/select';
 import { FormsModule } from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 
-  interface tagList {
+  interface TagList {
+    name: string;
     replace: boolean;
     values: Array<string>;
   };
 
 @Component({
   selector: 'app-tag-sets',
-  imports: [SelectModule,FormsModule,MatButtonModule],
+  imports: [SelectModule,FormsModule,MatButtonModule,MatInputModule],
   templateUrl: './tag-sets.html',
   styleUrl: './tag-sets.scss',
 })
@@ -21,7 +23,11 @@ export class TagSets {
 
   allItems:any;
   selectedItem:any;
-  tagList:tagList = {replace:true, values:[]}
+  selectedTagList: TagList|undefined;
+  tagList:TagList = {name: "", replace:true, values:[]};
+  savedTagLists: Array<TagList> = [];
+  currentListName: string = "";
+  existingLists = [];
 
     constructor(
     private jsonLoaderService: JsonLoaderService,
@@ -36,13 +42,67 @@ export class TagSets {
       .subscribe((data: any) => {
         this.allItems = data;
       });
+      this.refreshLocalstorageLists();
+      console.log(this.savedTagLists);
+
 
   }
+
+  refreshLocalstorageLists() {
+    this.savedTagLists = [];
+    const localCopy = JSON.parse(localStorage.getItem("saved-items") ?? "[]")
+      localCopy.forEach((element: string) => {
+        if (element.includes(".tags")) {
+          const localTagList = JSON.parse(localStorage.getItem(element) ?? "[]")
+          console.log(localTagList);
+          if (!Array.isArray(localTagList)) {
+            this.savedTagLists.push(localTagList);
+          }
+        }
+      });
+  }
+  onLocallyStoredSelectChanged(event:any) {
+    console.log(event);
+    this.tagList = event.value;
+    this.currentListName = event.value.name;
+  }
+
+  deleteList() {
+    localStorage.removeItem(this.currentListName+".tags");
+    this.tagList = {name: "", replace:true, values:[]};
+    this.currentListName = "";
+    this.selectedTagList = undefined;
+    this.savedTagLists = [];
+    this.refreshLocalstorageLists();
+    console.log(this.savedTagLists );
+  }
+
   addTag() {
     this.tagList.values.push("minecraft:"+this.selectedItem.name);
     let tempSet = new Set(this.tagList.values);
     this.tagList.values = Array.from(tempSet);
-    console.log(this.selectedItem);
-    console.log(this.tagList);
+    this.selectedItem = null;
   }
+
+  removeTag(value: string) {
+    this.tagList.values = this.tagList.values.filter(v => v !== value);
+  }
+
+  saveList() {
+    this.tagList.name = this.currentListName;
+    localStorage.setItem(this.currentListName+".tags", JSON.stringify(this.tagList));
+    let tempItems:any[]
+    if (localStorage.getItem("saved-items")) {
+      tempItems = JSON.parse(localStorage.getItem("saved-items")??"[]");
+    } else {
+      tempItems = [];
+    }
+    tempItems.push(this.currentListName+".tags");
+    let tempSet = new Set(tempItems);
+    
+    localStorage.setItem("saved-items", JSON.stringify(Array.from(tempSet.values())));
+    this.refreshLocalstorageLists(); 
+  }
+
+
 }
